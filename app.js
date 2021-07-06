618ef1284f1cb35744b0fc925d58354aa2fa8ece
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-06 16:39:04 -0700

    code clean-up

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// import functions and grab DOM elements
 import { supplies } from './supplies.js';
 import { renderSupplies } from './render-supplies.js';
 console.log(supplies);
-// initialize state
 
 const suppliesUL = document.getElementById('supplies');
 
@@ -10,7 +8,3 @@ for (let supply of supplies) {
     const suppliesLI = renderSupplies(supply);
     suppliesUL.appendChild(suppliesLI);
 }
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
